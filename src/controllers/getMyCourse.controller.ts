e7fe2fd5f088451e5312f5b83354d8030ecb1864
nokamui <223329114+nokamui123@users.noreply.github.com>
2025-08-05 00:36:48 +0700

    get only peroid_date is "23/06/2568-19/10/2568"

--- src/controllers/getMyCourse.controller.ts
+++ src/controllers/getMyCourse.controller.ts
@@ -30,21 +30,25 @@ export async function getMyCourse(
     ) {
       throw new Error("No course data available to save.");
     }
-    return res.data.results[0].course.map((course) => ({
-      sectionId: course.section_id, // Ensure this is unique
-      subjectCode: course.subject_code,
-      subjectNameTh: course.subject_name_th,
-      sectionCode: course.section_code,
-      sectionTypeTh: course.section_type_th,
-      stdStatusTh: course.std_status_th,
-      teacherName: course.teacher_name || null, // Handle optional field
-      timeFrom: course.time_from,
-      timeTo: course.time_to,
-      dayW: course.day_w,
-      roomNameTh: course.room_name_th,
-      sectionType: course.section_type,
-      stdCode,
-    }));
+    return (
+      res.data.results
+        .find((x) => x.peroid_date === "23/06/2568-19/10/2568")
+        ?.course.map((course) => ({
+          sectionId: course.section_id, // Ensure this is unique
+          subjectCode: course.subject_code,
+          subjectNameTh: course.subject_name_th,
+          sectionCode: course.section_code,
+          sectionTypeTh: course.section_type_th,
+          stdStatusTh: course.std_status_th,
+          teacherName: course.teacher_name || null, // Handle optional field
+          timeFrom: course.time_from,
+          timeTo: course.time_to,
+          dayW: course.day_w,
+          roomNameTh: course.room_name_th,
+          sectionType: course.section_type,
+          stdCode,
+        })) || null
+    );
   } catch (error) {
     return null;
   }
