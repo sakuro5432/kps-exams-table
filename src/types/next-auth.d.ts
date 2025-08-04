13d4357bcc8ee2ec2829bfd54eacaeb4245b4bec
nokamui <223329114+nokamui123@users.noreply.github.com>
2025-08-04 21:23:47 +0700

    backup

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,71 +1,3 @@
-// import { $Enums } from "@/lib/generated/prisma";
-// import NextAuth from "next-auth";
-// import { MyKuLoginProfile } from "./sign-in";
-// declare module "next-auth" {
-//   interface Session {
-//     user: {
-//       id: string;
-//       name: string;
-//       email: string;
-//       image?: string;
-//       campusNameTh: string;
-//       facultyNameTh: string;
-//       departmentNameTh: string;
-//       majorNameTh: string;
-//       studentStatusNameTh: string;
-//       studentYear: string;
-//       accesstoken: string;
-//       renewtoken: string;
-//       forceLogout?: boolean;
-//     };
-//   }
-//   interface Account {
-//     id: string;
-//     name: string;
-//     email: string;
-//     image?: string;
-//   }
-//   interface User {
-//     id: string;
-//     name: string;
-//     email?: string;
-//     image?: string;
-//     campusNameTh: string;
-//     facultyNameTh: string;
-//     departmentNameTh: string;
-//     majorNameTh: string;
-//     studentStatusNameTh: string;
-//     studentYear: string;
-//     accesstoken: string;
-//     renewtoken: string;
-//     forceLogout?: boolean;
-//   }
-//   interface Profile {
-//     id: string;
-//     name: string;
-//     email: string;
-//     image?: string;
-//     given_name?: string | null;
-//     family_name?: string | null;
-//   }
-// }
-// declare module "next-auth/jwt" {
-//   interface JWT {
-//     id: string;
-//     name: string;
-//     email: string;
-//     image?: string;
-//     campusNameTh: string;
-//     facultyNameTh: string;
-//     departmentNameTh: string;
-//     majorNameTh: string;
-//     studentStatusNameTh: string;
-//     studentYear: string;
-//     accesstoken: string;
-//     renewtoken: string;
-//     forceLogout?: boolean;
-//   }
-// }
 import { $Enums } from "@/lib/generated/prisma";
 import NextAuth from "next-auth";
 import { LoginProfile } from "./signIn.types";
@@ -79,6 +11,7 @@ declare module "next-auth" {
       forceLogout?: boolean;
       studentInfo: LoginProfile["student"];
       accesstoken: string;
+      role: $Enums.UserRole;
     };
   }
   interface Account {
@@ -95,6 +28,7 @@ declare module "next-auth" {
     forceLogout?: boolean;
     studentInfo: LoginProfile["student"];
     accesstoken: string;
+    role: $Enums.UserRole;
   }
   interface Profile {
     id: string;
@@ -114,5 +48,6 @@ declare module "next-auth/jwt" {
     forceLogout?: boolean;
     studentInfo: LoginProfile["student"];
     accesstoken: string;
+    role: $Enums.UserRole;
   }
 }
