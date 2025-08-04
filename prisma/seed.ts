c7cb99e02e082b3911297406362a410a050b0b04
nokamui <223329114+nokamui123@users.noreply.github.com>
2025-08-04 21:06:26 +0700

    added field `reportBy` for push.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -8,20 +8,25 @@ const folderPath = path.join(__dirname, "data");
 async function main() {
   const count = await prisma.examSchedule.count();
   if (count === 0) {
-    const data: Omit<
+    type ExpactedData = Omit<
       ExamSchedule,
-      "id" | "deletedAt" | "isCorrectCount" | "status" | "createdAt"
-    >[] = baseData.map((x) => ({
-      ...x,
-      date: new Date(x.date),
-    }));
+      "id" | "deletedAt" | "isCorrectCount" | "status" | "createdAt" | "stdCode"
+    >;
+    const data: ExpactedData[] = baseData.map(
+      (x) =>
+        ({
+          ...x,
+          date: new Date(x.date),
+          reportBy: "SYSTEM",
+        } as ExpactedData)
+    );
     const files = fs.readdirSync(folderPath).filter((x) => x !== "base.json");
     files.forEach((file) => {
       const r = fs.readFileSync(folderPath.concat("/", file), {
         encoding: "utf-8",
       });
       (JSON.parse(r) as (ExamSchedule & { date: number })[]).forEach((x) => {
-        data.push(x);
+        data.push({ ...x, reportBy: "SYSTEM" });
       });
     });
     await prisma.examSchedule.createMany({ data, skipDuplicates: true });
