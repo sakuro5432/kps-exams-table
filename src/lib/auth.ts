f6a0b6443e8b546da1badb0f24ec59f7f7c5e4b3
nokamui <223329114+nokamui123@users.noreply.github.com>
2025-08-04 21:21:55 +0700

    required query from db

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,11 +3,17 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth";
 import { decodeJwt } from "jose";
 import { MyKuUserTokenInterface } from "../types/signIn.types";
+import { prisma } from "./db";
 
-export async function Auth(noCondition: boolean = false) {
+export async function Auth() {
   const session = await getServerSession(authOptions);
-  if (!session || session.user.forceLogout) return null
-  return session;
+  if (!session || session.user.forceLogout) return null;
+  const data = await prisma.user.findUnique({
+    where: { stdCode: session?.user.studentInfo.stdCode },
+    select: { stdCode: true, stdId: true, role: true },
+  });
+  if (!data) return null;
+  return { ...data, session: session.user };
 }
 
 export function decodeAccessToken(token: string) {
