d8096d68408153d2266317a032428e5eccfd95a8
Sakuro <223329114+sakuro5432@users.noreply.github.com>
2025-10-10 12:45:42 +0700

    unuse validateSession() in Auth() because unnecessary

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,25 +3,10 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/authOptions";
 import { getServerSession } from "next-auth";
 import { decodeJwt } from "jose";
 import { MyKuUserTokenInterface } from "../types/signIn.types";
-import { validateSession } from "./session";
 
 export async function Auth() {
   const session = await getServerSession(authOptions);
   if (!session || session.user.forceLogout) return null;
-
-  // const data = await prisma.user.findUnique({
-  //   where: { stdCode: session?.user.studentInfo.stdCode },
-  //   select: { stdCode: true, stdId: true, role: true },
-  // });
-  // if (!data) return null;
-
-  const isValidSession = await validateSession(
-    session.user.id,
-    session.user.sessionId
-  );
-  if (!isValidSession) {
-    return null;
-  }
   return { session: session.user };
 }
 
